bff345c7ba622fb8d30299ae9b9e1b7f52cf54ab
Kofi <kofiasiedu@hotmail.co.uk>
2019-04-12 21:49:10 +0000

    created first tempalte

--- artist/views.py
+++ artist/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from models import Crafts
 
 def index(request):
+    #pull all crafts objects
     all_crafts =Crafts.objects.all()
-    html=''
-    for craft in all_crafts:
-        url='/craft/'+ str(craft.id)+ '/'
-        html+='<a href="'+url+'">craft.work_title</a><br/>'
-    return HttpResponse(html)
+    template = loader.get_template('artist/index.html')
+    context = {
+        'all_crafts':all_crafts,
+    }
+    
+    return HttpResponse(template.render(context,request))
     
 def detail(request):
    return HttpResponse("<h2>Details for Craft ID: "+ str(id) + "</h2>")
