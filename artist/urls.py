f96def9edba0a5be79c0674c061014e5e0a93f7e
Kofi <kofiasiedu@hotmail.co.uk>
2019-04-23 13:09:41 +0000

    configured views

--- artist/urls.py
+++ artist/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from . import views
 
 app_name = 'artist'
 
 urlpatterns = [
-
-
-
+    #two views poiting to the same view with different names
+    url('', views.index, name='starter'),
+    url('artist', views.index, name='index')
 ]
--- artist/views.py
+++ artist/views.py
@@ -1,4 +1,8 @@
 from django.views import generic
+from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Artist, GroupMember, Catalogue, Bug, Feature
+
+def index(request):
+    return render(request, 'artist/index.html',{})
 
