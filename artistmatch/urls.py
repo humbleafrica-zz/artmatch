654a116d0494067dfc19992afa508598b9ccbb48
Kofi <kofiasiedu@hotmail.co.uk>
2019-05-27 09:03:10 +0000

    venv commit

--- artistmatch/urls.py
+++ artistmatch/urls.py
@@ -1,7 +1,7 @@
 
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
+from django.contrib.auth import LoginView as auth_views
 from users import views as user_views
 
 urlpatterns = [
